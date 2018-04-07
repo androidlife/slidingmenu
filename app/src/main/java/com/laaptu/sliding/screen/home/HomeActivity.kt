797536d7d4068b0df7cbec15f7cceacd86318f6c
laaptu <sanimap@gmail.com>
2018-04-07 17:00:45 +1000

    Some minor refactoring

--- app/src/main/java/com/laaptu/sliding/screen/home/HomeActivity.kt
+++ app/src/main/java/com/laaptu/sliding/screen/home/HomeActivity.kt
@@ -13,9 +13,7 @@ import kotlinx.android.synthetic.main.app_bar_main.*
 class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     var selectedIndex: Int = R.id.nav_gallery
-    private val SAVE_SELECTED_INDEX = "SelectedIndex"
-    private val TAG_GALLERY = "GALLERY"
-    private val TAG_PLACES = "PLACES"
+    private val SELECTED_INDEX = "SelectedIndex"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,42 +26,41 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         if (savedInstanceState != null) {
-            selectedIndex = savedInstanceState.getInt(SAVE_SELECTED_INDEX, R.id.nav_gallery)
-            println("SELECTED INDEX ONRESTORE  =  " + selectedIndex)
+            selectedIndex = savedInstanceState.getInt(SELECTED_INDEX, R.id.nav_gallery)
         }
         changeSelectedIndex(selectedIndex, true)
         navigationView.setNavigationItemSelectedListener(this)
     }
 
     private fun changeSelectedIndex(newIndex: Int, setSelectedMenu: Boolean) {
+        val fragTag = newIndex.toString()
         when (newIndex) {
             R.id.nav_gallery -> {
                 supportActionBar?.title = getString(R.string.gallery)
-                if (supportFragmentManager.findFragmentByTag(TAG_GALLERY) == null)
+
+                if (supportFragmentManager.findFragmentByTag(fragTag) == null)
                     supportFragmentManager.beginTransaction().replace(
-                            R.id.container, GalleryFragment(), TAG_GALLERY
+                            R.id.container, GalleryFragment(), fragTag
                     ).commit()
                 if (setSelectedMenu)
                     navigationView.setCheckedItem(R.id.nav_gallery)
             }
             R.id.nav_location -> {
                 supportActionBar?.title = getString(R.string.places)
-                if (supportFragmentManager.findFragmentByTag(TAG_PLACES) == null)
+                if (supportFragmentManager.findFragmentByTag(fragTag) == null)
                     supportFragmentManager.beginTransaction().replace(
-                            R.id.container, PlacesFragment(), TAG_PLACES
+                            R.id.container, PlacesFragment(), fragTag
                     ).commit()
                 if (setSelectedMenu)
                     navigationView.setCheckedItem(R.id.nav_location)
             }
         }
         selectedIndex = newIndex
-        println("SELECTED INDEX @ CHANGE SELECTED = " + selectedIndex)
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
-        outState?.putInt(SAVE_SELECTED_INDEX, selectedIndex)
-        println("SELECTED INDEX ONSAVE = " + selectedIndex)
+        outState?.putInt(SELECTED_INDEX, selectedIndex)
     }
 
 
