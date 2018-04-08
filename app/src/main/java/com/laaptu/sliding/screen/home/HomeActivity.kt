9cbb7c77beb5d5b123b73776edb46a37cd596a78
laaptu <sanimap@gmail.com>
2018-04-08 20:47:12 +1000

    Saving the state of gallery on HomeActivity

--- app/src/main/java/com/laaptu/sliding/screen/home/HomeActivity.kt
+++ app/src/main/java/com/laaptu/sliding/screen/home/HomeActivity.kt
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import com.laaptu.sliding.R
 import com.laaptu.sliding.screen.home.gallery.GalleryFragment
+import com.laaptu.sliding.screen.home.gallery.ViewStateGallery
 import com.laaptu.sliding.screen.home.places.PlacesFragment
 import com.laaptu.sliding.screen.home.places.ViewStatePlaces
 import kotlinx.android.synthetic.main.activity_home.*
@@ -17,6 +18,7 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     private var selectedIndex: Int = R.id.nav_gallery
     private var viewStatePlaces = ViewStatePlaces()
+    private var viewStateGallery = ViewStateGallery()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +33,7 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         if (savedInstanceState != null) {
             selectedIndex = savedInstanceState.getInt(SELECTED_INDEX)
             viewStatePlaces = savedInstanceState.getParcelable(VIEW_STATE_PLACES)
+            viewStateGallery = savedInstanceState.getParcelable(VIEW_STATE_GALLERY)
         }
         changeSelectedIndex(selectedIndex, true)
         navigationView.setNavigationItemSelectedListener(this)
@@ -43,7 +46,7 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 supportActionBar?.title = getString(R.string.gallery)
                 if (supportFragmentManager.findFragmentByTag(fragTag) == null)
                     supportFragmentManager.beginTransaction().replace(
-                            R.id.container, GalleryFragment(), fragTag
+                            R.id.container, GalleryFragment.getInstance(viewStateGallery), fragTag
                     ).commit()
                 if (setSelectedMenu)
                     navigationView.setCheckedItem(R.id.nav_gallery)
@@ -65,6 +68,7 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onSaveInstanceState(outState)
         outState?.putInt(SELECTED_INDEX, selectedIndex)
         outState?.putParcelable(VIEW_STATE_PLACES, viewStatePlaces)
+        outState?.putParcelable(VIEW_STATE_GALLERY, viewStateGallery)
     }
 
 
@@ -94,6 +98,7 @@ class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val fragment = supportFragmentManager.findFragmentByTag(fragId.toString()) ?: return
         when (fragId) {
             R.id.nav_location -> viewStatePlaces = (fragment as PlacesFragment).getViewState().copy()
+            R.id.nav_gallery -> viewStateGallery = (fragment as GalleryFragment).getViewState().copy()
         }
 
     }
